b3f28203667d20d29c629564c52daf7a5f50d17d
Sevda Shukrieva <sevda.shukrieva@xdesign.com>
2023-02-26 23:44:24 +0000

    add seed specific typescript config and create mock data for the database

---
+++ prisma/seed.ts
@@ -0,0 +1,70 @@
+// Seed script ( a piece of code ) that inserts fake data into our dev DB so we can use it for development
+// This will create a mock user with some projects and tasks that we can use to signup and test the application.
+
+import { db } from "@/lib/db";
+import { TASK_STATUS } from "@prisma/client";
+
+const getRandomTaskStatus = () => {
+  const statuses = [
+    TASK_STATUS.COMPLETED,
+    TASK_STATUS.NOT_STARTED,
+    TASK_STATUS.STARTED,
+  ];
+  return statuses[Math.floor(Math.random() * statuses.length)];
+};
+
+// use client to upsert (find or create) the user
+async function main() {
+  const user = await db.user.upsert({
+    where: { email: "user@email.com" },
+    update: {},
+    create: {
+      email: "user@email.com",
+      firstName: "User",
+      lastName: "Person",
+      password: "password",
+      projects: {
+        create: new Array(5).fill(1).map((_, i) => ({
+          name: `Project ${i}`,
+          due: new Date(2022, 11, 25),
+        })),
+      },
+    },
+    include: {
+      projects: true,
+    },
+  });
+
+  const tasks = await Promise.all(
+    user.projects.map((project) =>
+      db.task.createMany({
+        data: new Array(10).fill(1).map((_, i) => {
+          return {
+            name: `Task ${i}`,
+            ownerId: user.id,
+            projectId: project.id,
+            description: `Everything that describes Task ${i}`,
+            status: getRandomTaskStatus(),
+          };
+        }),
+      })
+    )
+  );
+
+  console.log({ user, tasks });
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  })
+  .catch(async (e) => {
+    console.log(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
